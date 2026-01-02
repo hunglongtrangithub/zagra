32b0cf71cf1108bf919fddf37213f9c93ee02563
Hung Tran <hunglongtran2004@gmail.com>
2026-01-01 23:37:15 -0500

    feat: restrict possible vector data type and dimension with enums

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,14 @@
 const std = @import("std");
 const zagra = @import("zagra");
 
+const Vec3f = zagra.Vector(.f32, .D3);
+
 pub fn main() !void {
     std.debug.print("ZAGRA executable\n", .{});
-    const Vec3f = zagra.Vector(i32, 3);
 
-    const v1 = Vec3f.init([3]i32{ 1, 2, 3 });
-    const v2 = Vec3f.init([3]i32{ 6, 22, 43 });
-
-    const sqdist = Vec3f.sqdist(&v1, &v2);
+    const v1 = Vec3f.init([3]f32{ 1, 2, 3 });
+    const v2 = Vec3f.init([3]f32{ 6, 22, 43 });
+    const sqdist = v1.sqdist(&v2);
 
     std.debug.print("Squared Distance: {d}\n", .{sqdist});
 }
--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,12 @@
 //! root source file of the zagra module
 const std = @import("std");
 
-const DataType = enum { f32, i32 };
-const Dimension = enum(u32) { D2 = 2, D3 = 3, D4 = 4 };
+const DataType = enum {
+    /// Zig's f32 type
+    f32,
+    /// Zig's i32 type
+    i32,
+};
 
 fn getType(comptime dt: DataType) type {
     return switch (dt) {
@@ -11,27 +15,54 @@ fn getType(comptime dt: DataType) type {
     };
 }
 
-fn getDim(comptime dim: Dimension) u32 {
-    return @intFromEnum(dim);
+const Dimension = enum(usize) {
+    /// 3-dimensional vector
+    D3 = 3,
+    /// 32-dimensional vector
+    D32 = 32,
+    /// 64-dimensional vector
+    D64 = 64,
+    /// 128-dimensional vector
+    D128 = 128,
+};
+
+fn getDim(comptime dim: Dimension) usize {
+    return switch (dim) {
+        .D3 => 3,
+        .D32 => 32,
+        .D64 => 64,
+        .D128 => 128,
+    };
 }
-pub fn Vector(comptime T: type, comptime N: u32) type {
+
+/// A generic N-dimensional vector type with elements of type T
+pub fn Vector(comptime data_type: DataType, comptime dimension: Dimension) type {
+    const T = getType(data_type);
+    const N = getDim(dimension);
     return struct {
         data: [N]T,
 
         const Self = @This();
 
+        /// Initialize the vector with an array of values
+        /// `N` must match the dimension of the vector.
+        /// `T` must match the data type of the vector.
         pub fn init(values: [N]T) Self {
             return Self{ .data = values };
         }
 
+        /// Access element at index `idx`
         pub fn at(self: *const Self, idx: usize) T {
+            std.debug.assert(idx < N);
             return self.data[idx];
         }
 
-        pub fn sqdist(a: *const Self, b: *const Self) T {
+        /// Calculate squared distance between two vectors
+        pub fn sqdist(v1: *const Self, v2: *const Self) T {
             var sum: T = 0;
-            for (a.data, b.data) |x, y| {
-                const diff = x - y;
+            // TODO: Optimize with SIMD later
+            for (v1.data, v2.data) |e1, e2| {
+                const diff = e1 - e2;
                 sum += diff * diff;
             }
             return sum;
