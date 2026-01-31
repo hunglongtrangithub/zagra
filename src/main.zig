4538046193b8e90eba2f036e1f2a003939668011
Hung Tran <hunglongtran2004@gmail.com>
2026-01-31 16:14:43 -0500

    feat(nn_descent): first working version of block processing

--- src/main.zig
+++ src/main.zig
@@ -43,7 +43,7 @@ pub fn main() !void {
 
     // Dataset configuration constants
     const vector_length: usize = 128;
-    const vector_count: usize = 10_000_000;
+    const vector_count: usize = 5_000_000;
     const npy_file_name = "dataset.npy";
     const element_type = f32;
     var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
