4f33fadcefe95191786c1ec046b44a3c01377ce8
Hung Tran <hunglongtran2004@gmail.com>
2026-02-08 16:35:29 -0500

    feat(main): allocate vectors buffer straight in memory

--- src/main.zig
+++ src/main.zig
@@ -70,105 +70,39 @@ pub fn main() !void {
     try stdout.print("Using block processing for training? {any}\n", .{block_processing});
 
     // Dataset configuration constants
-    const vector_length: usize = 128;
-    const npy_file_name = "dataset.npy";
-    const element_type = f32;
+    const T = f32; // Element type
+    const N: usize = 128; // Vector length
 
-    try stdout.print("Saving dataset with {} {}-D vectors to {s}\n", .{ vector_count, vector_length, npy_file_name });
+    try stdout.print("Creating dataset with {} {}-D vectors\n", .{ vector_count, N });
     try stdout.flush();
 
-    // Open file to write Npy array to
-    const npy_file_to_write = try std.fs.cwd().createFile(npy_file_name, .{});
-    var file_buffer: [8192]u8 = undefined;
-    var file_writer = std.fs.File.Writer.init(npy_file_to_write, &file_buffer);
-
-    // Initialize the Npy array
-    const Array = znpy.array.StaticArray(element_type, 2);
-    const array = Array.init(
-        [2]usize{ vector_count, vector_length },
-        znpy.shape.Order.C,
-        allocator,
-    ) catch |e| {
-        std.debug.print("Error initializing array: {}", .{e});
+    // Allocate aligned buffer for vectors
+    const vectors_buffer = allocator.alignedAlloc(
+        T,
+        std.mem.Alignment.@"64",
+        vector_count * N,
+    ) catch {
+        std.debug.print("Dataset size too large. Please try smaller number of vectors.\n", .{});
         return;
     };
-    defer array.deinit(allocator);
-    // Fill array with increasing numbers
-    for (0..vector_count) |i| {
-        for (0..vector_length) |j| {
-            array.at([2]usize{ i, j }).?.* = @floatFromInt(i * vector_length + j);
-        }
+    defer allocator.free(vectors_buffer);
+
+    // Fill with increasing numbers
+    for (0..vectors_buffer.len) |i| {
+        vectors_buffer[i] = @floatFromInt(i);
     }
-    // Write file to disk
-    array.writeAll(&file_writer.interface, allocator) catch |e| {
-        std.debug.print("Error writing array to disk: {}", .{e});
-        return;
+
+    // Create dataset from buffer
+    const Dataset = zagra.dataset.Dataset(T, N);
+    const dataset = Dataset{
+        .data_buffer = vectors_buffer,
+        .len = vector_count,
     };
-    try file_writer.interface.flush();
-
-    npy_file_to_write.close();
-    try stdout.print("Wrote Npy array to {s}\n", .{npy_file_name});
-    try stdout.flush();
-
-    try stdout.print("Reading the Npy file to dataset\n", .{});
-
-    const npy_file_to_read = try std.fs.cwd().openFile(npy_file_name, .{});
-    const Dataset = zagra.dataset.Dataset(element_type, vector_length);
-
-    // Read the file
-    // var file_buffer: [8192]u8 = undefined;
-    var file_reader = std.fs.File.Reader.init(npy_file_to_read, &file_buffer);
-
-    // Read the Npy file content into the dataset
-    const dataset = Dataset.fromNpyFileReader(&file_reader.interface, allocator) catch |e| {
-        std.debug.print("Error creating the dataset from the array file: {}", .{e});
-        return;
-    };
-    defer dataset.deinit(allocator);
-
-    // // Get the file buffer using mmap
-    //
-    // // Get file size
-    // const file_stat = try npy_file_to_read.stat();
-    // const read_size = std.math.cast(usize, file_stat.size) orelse {
-    //     std.debug.print("File size is too large to map\n", .{});
-    //     return;
-    // };
-    // if (read_size == 0) {
-    //     std.debug.print("File is empty, nothing to read\n", .{});
-    //     return;
-    // }
-    //
-    // // Read all file contents into memory using mmap
-    // const file_buffer = try std.posix.mmap(
-    //     null,
-    //     read_size,
-    //     std.posix.PROT.READ,
-    //     std.posix.system.MAP{ .TYPE = .PRIVATE },
-    //     npy_file_to_read.handle,
-    //     0,
-    // );
-    // defer std.posix.munmap(file_buffer);
-    // if (file_buffer.len != read_size) {
-    //     std.debug.print("Mapped size does not match file size.\n", .{});
-    //     return;
-    // }
-    //
-    // // Form the dataset using the file buffer
-    // const dataset = try Dataset.fromNpyFileBuffer(file_buffer, allocator);
 
     std.debug.assert(dataset.len == vector_count);
 
-    npy_file_to_read.close();
-
-    // for (0..dataset.len) |i| {
-    //     const vector = dataset.getUnchecked(i);
-    //     try stdout.print("vector {}: {any}\n", .{ i, vector.data });
-    // }
-    // try stdout.flush();
-
     // Do NN-Descent
-    const NNDescent = zagra.graphs.nn_descent.NNDescent(element_type, vector_length);
+    const NNDescent = zagra.graphs.nn_descent.NNDescent(T, N);
     var training_config = zagra.graphs.nn_descent.TrainingConfig.init(
         4,
         vector_count,
@@ -198,15 +132,4 @@ pub fn main() !void {
 
     try stdout.print("Training for {} vectors took: {}s\n", .{ dataset.len, elapsed_time_s });
     try stdout.flush();
-
-    // try stdout.print("Graph after training:\n", .{});
-    // for (0..nn_descent.neighbors_list.num_nodes) |node_id| {
-    //     try stdout.print("Node {}:\n", .{node_id});
-    //     for (0..nn_descent.neighbors_list.num_neighbors_per_node) |neighbor_idx| {
-    //         const neighbor_id: isize = nn_descent.neighbors_list.getEntryFieldSlice(node_id, .neighbor_id)[neighbor_idx];
-    //         const neighbor_distance: element_type = nn_descent.neighbors_list.getEntryFieldSlice(node_id, .distance)[neighbor_idx];
-    //         try stdout.print(" " ** 4 ++ "Neighbor {}: distance {}\n", .{ neighbor_id, neighbor_distance });
-    //     }
-    // }
-    // try stdout.flush();
 }
